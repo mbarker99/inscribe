d35260efcc8b56df83276a48d09ef7b5abbcb0ea
mbarker99 <michael.barker1999@gmail.com>
2024-10-08 17:53:09 -0400

    Fix warnings and error.

--- app/src/main/java/com/embarkapps/inscribe/MainActivity.kt
+++ app/src/main/java/com/embarkapps/inscribe/MainActivity.kt
@@ -7,13 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.embarkapps.inscribe.presentation.ui.theme.InscribeTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
+        enableEdgeToEdge()
         setContent {
             InscribeTheme {
                 Column(verticalArrangement = Arrangement.Center,
